d4c15b29f20ea51a793b68a772a3b39d6f3129a5
DatCoder <84683238+DatCoder464@users.noreply.github.com>
2022-11-23 20:24:20 -0600

    glass bom partially set up

--- src/main/java/io/github/unknowntexture/createkaboom/item/Moditems.java
+++ src/main/java/io/github/unknowntexture/createkaboom/item/Moditems.java
@@ -10,6 +10,11 @@ import net.minecraftforge.registries.RegistryObject;
 public class Moditems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, CreateKaboom.MOD_ID);
+    public static final RegistryObject<Item> GLASS_SHELL = ITEMS.register("glass_shell",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.CREATE_KABOOM_TAB)));
+    
+    public static final RegistryObject<SequencedAssemblyItem> INCOMPLET_GLASS_BOMB = ITEMS.register("incomplete_glass_bomb",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.CREATE_KABOOM_TAB)));
 
     public static final RegistryObject<Item> IRON_BOMB = ITEMS.register("iron_bomb",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.CREATE_KABOOM_TAB)));
