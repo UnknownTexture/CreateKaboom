611ceb2e22a20602d998f7d1af71a99d53ffc101
DatCoder <84683238+DatCoder464@users.noreply.github.com>
2022-11-23 21:38:50 -0600

    Create GPSand.java

---
+++ src/main/java/io/github/unknowntexture/createkaboom/liquids/GPSand.java
@@ -0,0 +1,12 @@
+
+    public static final RegistryObject<ForgeFlowingFluid> GUNPOWDER_SAND
+            = FLUIDS.register("GUNPOWDER_SAND_FLUID", () -> new ForgeFlowingFluid.Source(FluidRegistry.GUNPOWDER_SAND_PROPERTIES));
+
+    public static final RegistryObject<ForgeFlowingFluid> GUNPOWDER_SAND_FLOWING
+            = FLUIDS.register("GUNPOWDER_SAND_FLOWING", () -> new ForgeFlowingFluid.Flowing(FluidRegistry.GUNPOWDER_SAND_PROPERTIES));
+
+
+    public static final ForgeFlowingFluid.Properties GUNPOWDER_SAND_PROPERTIES = new ForgeFlowingFluid.Properties(
+            () -> GUNPOWDER_SAND_FLUID.get(), () -> GUNPOWDER_SAND_FLOWING.get(), FluidAttributes.builder(GUNPOWDER_SAND_FLUID_STILL, null)
+            .density(15).luminosity(2).viscosity(7.5).sound(SoundEvents.HONEY_DRINK)
+            ).slopeFindDistance(2).levelDecreasePerBlock(10);
