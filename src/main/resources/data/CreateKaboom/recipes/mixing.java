16b19b4c92d355ad6445d89933c0149f263d7cef
DatCoder <84683238+DatCoder464@users.noreply.github.com>
2022-11-23 22:20:21 -0600

    Update mixing.java

--- src/main/resources/data/CreateKaboom/recipes/mixing.java
+++ src/main/resources/data/CreateKaboom/recipes/mixing.java
@@ -1,1 +1,22 @@
-
+{
+  "type": "create:mixing",
+  "ingredients": [
+    {
+      "item": "forge:food"
+    },
+    {
+      "fluid": "minecraft:water",
+      "nbt": {},
+      "amount": 500
+    }
+  ],
+  "results": [
+    {
+      "fluid": "createkaboom:alcohol",
+      "nbt": {},
+      "amount": 750
+    }
+  ],
+  "heatRequirement": "heated",
+  "processingTime": 10000
+}
