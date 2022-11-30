d6cfa3b3c7c739dbf6d1b0f6218f294cda54ad80
Amiran <protonguy9@gmail.com>
2022-11-30 22:43:45 +0400

    Made bomb throwable

--- src/main/java/io/unknowntexture/createkaboom/throwable/ThrowableBombProjectile.java
+++ src/main/java/io/unknowntexture/createkaboom/throwable/ThrowableBombProjectile.java
@@ -1,8 +1,52 @@
 package io.unknowntexture.createkaboom.throwable;
 
-public class ThrowableBombProjectile {
+import io.unknowntexture.createkaboom.item.ModItems;
+import net.minecraft.core.particles.ItemParticleOption;
+import net.minecraft.core.particles.ParticleOptions;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.projectile.ThrowableItemProjectile;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.EntityHitResult;
+
+public class ThrowableBombProjectile extends ThrowableItemProjectile {
 
 
+    public ThrowableBombProjectile(EntityType<? extends ThrowableItemProjectile> p_37442_, Level p_37443_) {
+        super(p_37442_, p_37443_);
+    }
 
+    @Override
+    protected Item getDefaultItem() {
+        //If it doesn't work, try: ModItems.IRON_BOMB.get();
+        return null;
+    }
+
+
+    public ThrowableBombProjectile(Level level, LivingEntity livingEntity) {
+        super(EntityType.SNOWBALL, livingEntity, level);
+    }
+
+    private ParticleOptions getParticle() {
+        ItemStack itemstack = this.getItemRaw();
+        return (ParticleOptions)(itemstack.isEmpty() ? ParticleTypes.ITEM_SNOWBALL : new ItemParticleOption(ParticleTypes.ITEM, itemstack));
+    }
+
+    public void handleEntityEvent(byte b) {
+        if (b == 3) {
+            ParticleOptions particleoptions = this.getParticle();
+
+            for(int i = 0; i < 8; ++i) {
+                this.level.addParticle(particleoptions, this.getX(), this.getY(), this.getZ(), 0.0D, 0.0D, 0.0D);
+            }
+        }
+
+    }
+
+    protected void onHitEntity(EntityHitResult hitResult) {
+    }
 
 }
