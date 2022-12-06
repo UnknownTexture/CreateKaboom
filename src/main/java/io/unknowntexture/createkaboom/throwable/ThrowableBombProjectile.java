e87ee487f097f00a1832b759a87e4b11ec62cf26
Amiran <protonguy9@gmail.com>
2022-12-06 17:32:28 +0400

    Made bomb explode (finish or configure it somehow pls)

--- src/main/java/io/unknowntexture/createkaboom/throwable/ThrowableBombProjectile.java
+++ src/main/java/io/unknowntexture/createkaboom/throwable/ThrowableBombProjectile.java
@@ -1,16 +1,29 @@
 package io.unknowntexture.createkaboom.throwable;
 
 import io.unknowntexture.createkaboom.item.ModItems;
+import net.minecraft.client.particle.Particle;
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.particles.ItemParticleOption;
 import net.minecraft.core.particles.ParticleOptions;
 import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.item.PrimedTnt;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.projectile.Snowball;
 import net.minecraft.world.entity.projectile.ThrowableItemProjectile;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Explosion;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.TntBlock;
+import net.minecraft.world.level.gameevent.GameEvent;
+import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.EntityHitResult;
+
+import javax.annotation.Nullable;
 
 public class ThrowableBombProjectile extends ThrowableItemProjectile {
 
@@ -47,6 +60,19 @@ public class ThrowableBombProjectile extends ThrowableItemProjectile {
     }
 
     protected void onHitEntity(EntityHitResult hitResult) {
+        throwableBombExplode();
     }
 
+    @Override
+    protected void onHitBlock(BlockHitResult p_37258_) {
+        super.onHitBlock(p_37258_);
+        throwableBombExplode();
+
+    }
+
+    protected void throwableBombExplode() {
+        float f = 2.0F;
+        //IDK is this radius or damage
+        this.level.explode(this, this.getX(), this.getY(0.0625D), this.getZ(), f, Explosion.BlockInteraction.NONE);
+    }
 }
